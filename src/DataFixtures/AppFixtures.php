6b8ae2b31490a2eb39019370d9e1341e1b2f1e8e
pheldu95 <phelps.duncan@gmail.com>
2025-10-02 17:44:53 -0500

    created User entity and data fixtures to add users to db

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,10 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        UserFactory::createOne([
+            'email' => 'phelps.duncan@gmail.com'
+        ]);
 
-        $manager->flush();
+        UserFactory::createMany(10);
     }
 }
