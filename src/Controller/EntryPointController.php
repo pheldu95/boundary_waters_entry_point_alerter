0414798c8e69c96db2f6e2d9f5ef20cd2dd4f521
pheldu95 <phelps.duncan@gmail.com>
2025-10-06 20:18:12 -0500

    removed old entry points template and route

--- src/Controller/EntryPointController.php
+++ src/Controller/EntryPointController.php
@@ -10,15 +10,6 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class EntryPointController extends AbstractController
 {
-    #[Route('/entry_points', name: 'app_entry_points_list')]
-    public function list(EntryPointRepository $entryPointRepository): Response
-    {
-        $entryPoints = $entryPointRepository->findAll();
-        return $this->render('entryPoint/entryPoints.html.twig', [
-            'entryPoints' => $entryPoints,
-        ]);
-    }
-
     #[Route('/entry-point/{id}', name: 'app_entry_point_show', requirements: ['id' => '[A-Za-z0-9]+'])]
     public function show(int $id, EntryPointRepository $entryPointRepository)
     {
