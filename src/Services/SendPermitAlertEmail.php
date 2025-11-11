992a83c5222e8d3db7646abb8e01d89eb7d0b528
pheldu95 <phelps.duncan@gmail.com>
2025-11-11 13:34:18 -0600

    created SendPermitAlertEmail service

---
+++ src/Services/SendPermitAlertEmail.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+namespace App\Services;
+
+use App\Entity\PermitWatch;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+
+class SendPermitAlertEmail
+{
+    public function __construct(
+        private MailerInterface $mailer, 
+        private ?LoggerInterface $logger = null
+    )
+    {
+        
+    }
+
+    /**
+     * Send a permit alert email.
+     *
+     * @param string $emailAddress Recipient e-mail address
+     * @param PermitWatch  $permitWatch
+     *
+     * @return bool
+     */
+    public function sendPermitAlert(string $emailAddress, PermitWatch $permitWatch): bool
+    {
+        $this->logger->info('Sending permit alert email to ' . $emailAddress);
+
+        $email = (new Email())
+            ->from('alert@entry-point-alerter.com')
+            ->to($emailAddress)
+            ->subject($permitWatch->getEntryPoint()->getName() . ' Permit Available!')
+            ->text('A permit has become available at ' . $permitWatch->getEntryPoint()->getName() . 
+                ' on ' . $permitWatch->getTargetDate()->format('Y-m-d') . '.')
+        ;
+        
+        try {
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            if ($this->logger) {
+                $this->logger->error('Failed to send permit alert email: ' . $e->getMessage());
+            }
+            return false;
+        }
+
+        return true;
+    }
+}
