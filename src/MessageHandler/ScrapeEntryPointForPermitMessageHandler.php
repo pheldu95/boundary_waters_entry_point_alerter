d34ffb2a962780408fe989e7d7924140a3a5c72a
pheldu95 <phelps.duncan@gmail.com>
2025-11-10 21:28:46 -0600

    composer require symfony/scheduler

--- src/MessageHandler/ScrapeEntryPointForPermitMessageHandler.php
+++ src/MessageHandler/ScrapeEntryPointForPermitMessageHandler.php
@@ -15,7 +15,7 @@ class ScrapeEntryPointForPermitMessageHandler
 
     public function __invoke(ScrapeEntryPointForPermitMessage $message)
     {
-        $this->logger->emergency('ScrapeEntryPointForPermitMessage received!');
+        $this->logger->info('ScrapeEntryPointForPermitMessage received!');
         $this->logger->info('Message received!', [
             'user' => $message->getUser()->getEmail(),
             'entryPoint' => $message->getPermitWatch()->getEntryPoint(),
@@ -24,12 +24,13 @@ class ScrapeEntryPointForPermitMessageHandler
 
         //Just to see that it's working
         dump('Handler processing: ' . $message->getPermitWatch()->getEntryPoint()->getName());
+        dump('Permit target date: ' . $message->getPermitWatch()->getTargetDate()->format('Y-m-d'));
 
         //Todo: Use actual permit watch date in the url
         $webScrapingClient = new WebScrapingClient();
         $result = $webScrapingClient;
         $result = $webScrapingClient->scrapeJson('https://www.recreation.gov/api/permits/233396/availability/month?start_date=2026-01-01T00:00:00.000Z&commercial_acct=false');
 
-        dump($result);
+        // dump($result);
     }
 }
